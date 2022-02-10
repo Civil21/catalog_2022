4dfb38ff8ad81b61a74095a24acf042c5bc54cff
civil21 <diusolexander@gmail.com>
2022-02-10 23:04:43 +0200

    lesson 2

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   root to: 'pages#index'
 
   resources :products,only: %i[index show]
+  resources :categories,only: %i[show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
